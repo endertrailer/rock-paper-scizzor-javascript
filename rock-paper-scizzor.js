53e4fba3244162ee5347dafb25bf86e86f45640f
endertrailer <anismasoom80@gmail.com>
2023-07-01 17:43:47 +0530

    rewrote the original js and html and added css

--- rock-paper-scizzor.js
+++ rock-paper-scizzor.js
@@ -1,78 +1,101 @@
-
 function getComputerChoice() {
-let min = 1;
-let max = 3;
-let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-if (randomNumber === 1){
-    return 'rock';
-}
-else if(randomNumber === 2){
-    return  'paper';
-}
-else{
-    return 'scizzor';
-}
-}
-const computerSelection = getComputerChoice();
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    if (playerSelection === computerSelection){
-        return 'Tie!';
-    }
-    else if (playerSelection === "rock"){
-        if (computerSelection === 'paper'){
-            return 'lose';
-        }
-        else if (computerSelection === 'scizzor'){
-            return 'win';
-        }
-    }
-    else if (playerSelection === "paper"){
-        if (computerSelection === 'scizzor'){
-            return 'lose';
-        }
-        else if (computerSelection === 'rock'){
-            return 'win';
-        }
-    }
-    else if (playerSelection === "scizzor"){
-        if (computerSelection ==='rock'){
-            return 'lose';
-        }
-        else if (computerSelection === 'paper'){
-            return 'win';
-        }
-    }
-    else {
-        return 'invalid input';
+    let min = 1;
+    let max = 3;
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    if (randomNumber === 1) {
+      return 'rock';
+    } else if (randomNumber === 2) {
+      return 'paper';
+    } else {
+      return 'scissor';
     }
   }
-   function game(){
-    let computerWins = 0;
-    let playerWins = 0;
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt('enter choice: ');
-        let round = playRound(playerSelection, computerSelection)
-        if (round === 'win'){
-            playerWins = playerWins + 1;
-        }
-        else if (round === 'lose'){
-            computerWins = computerWins + 1;
-        }
-        else{
-            return 'invalid input';
-        }
-    }
-    if (computerWins === playerWins){
-        return 'Its a tie!';
-    }
-    else if (computerWins >= playerWins){
-        return 'you lose';
-    }
-    else{
-        return 'you win';
-    }
-   }
   
- game()
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    
+    if (playerSelection === computerSelection) {
+      return 'Tie!';
+    } else if (playerSelection === 'rock') {
+      if (computerSelection === 'paper') {
+        return 'lose';
+      } else if (computerSelection === 'scissor') {
+        return 'win';
+      } else {
+        return;
+      }
+    } else if (playerSelection === 'paper') {
+      if (computerSelection === 'scissor') {
+        return 'lose';
+      } else if (computerSelection === 'rock') {
+        return 'win';
+      } else {
+        return;
+      }
+    } else if (playerSelection === 'scissor') {
+      if (computerSelection === 'rock') {
+        return 'lose';
+      } else if (computerSelection === 'paper') {
+        return 'win';
+      } else {
+        return;
+      }
+    } else {
+      return 'invalid input';
+    }
+  }
+  
+  let round = 0;
+  let computerWins = 0;
+  let playerWins = 0;
+  const result = document.querySelector('.result');
+  const rock = document.querySelector('.rock');
+  const paper = document.querySelector('.paper');
+  const scissor = document.querySelector('.scissor');
+  
+  function updateResult() {
+    if (round === 5) {
+      if (computerWins === playerWins) {
+        result.textContent = 'It\'s a tie!';
+      } else if (computerWins > playerWins) {
+        result.textContent = 'You lose!';
+      } else {
+        result.textContent = 'You win!';
+      }
+      round = 0;
+      computerWins = 0;
+      playerWins = 0;
+    }
+  }
+  
+  rock.addEventListener('click', function() {
+    if (playRound('rock', getComputerChoice()) === 'win') {
+      playerWins++;
+    } else {
+      computerWins++;
+    }
+    round++;
+    updateResult();
+  });
+  
+  paper.addEventListener('click', function() {
+    if (playRound('paper', getComputerChoice()) === 'win') {
+      playerWins++;
+    } else {
+      computerWins++;
+    }
+    round++;
+    updateResult();
+  });
+  
+  scissor.addEventListener('click', function() {
+    if (playRound('scissor', getComputerChoice()) === 'win') {
+      playerWins++;
+    } else {
+      computerWins++;
+    }
+    round++;
+    updateResult();
+  });
   
